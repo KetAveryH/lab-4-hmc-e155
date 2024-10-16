3d4d03aea4c7f6a9fe5d57ce2dacc4c91f36993d
Ket Hollingsworth <khollingsworth@g.hmc.edu>
2024-10-16 15:29:23 -0700

    TIM2 Init

--- lib/STM32L432KC_TIM.c
+++ lib/STM32L432KC_TIM.c
@@ -62,10 +62,76 @@ initTIM2(uint32_t freq) {
     the given frequency "freq" in hertz. 
     
     If you input a frequency of 0, this function should instead turn off  */
+  
+    //-----------------------// Timx control register 1 | TIMx_CR1
 
+    // Counter Disable bit 0
+    TIM2->CR1 &= 0b0; 
+    
+    // UIFREMAP bit 11
+    TIM2->CR1 &= ~(0b1 << 11);
+
+    // Clock Division bits 9:8
+    TIM2->CR1 &= ~(0b11 << 9);  // No Division 00
+
+    // ARPE Auto-reload preload enable bit 7
+    TIM2-> CR1 &= ~(0b1 <<7); // Auto-reload preload disabled
+    //TIM2->CR1 |= (0b1 <<7);
+
+    // CMS Center-aligned mode selection bits 6:5
+    TIM2->CR1 &= ~(0b11 << 7);  // 00: Edge-aligned mode
+
+    // DIR Counter Direction bit 4
+    TIM2->CR1 &= ~(0b1 << 4);
+
+    // OPM One Pulse Mode bit 3
+    TIM2->CR1 &= ~(0b1 << 3);
+
+    //-----------------------// TIMx control register 2 | TIMx_CR2
+
+    // TI1S Selection bit 7
+    TIM2->CR2 &= ~(0b1 << 7); // 0 TIMx_CH1 pin is connected to TI1 input
+
+    // MMS[2:0] Master Mode Selection bits 6:4
+    TIM2->CR2 &= ~(0b111 << 6);
+
+    // CCDS Capture/Compare DMA Selection bit 3
+    TIM2->CR2 &= ~(0b1 << 3);
+
+    //-----------------------// TIMx slave mode control register | TIMx_SMCR
+    
+
+    //-----------------------// TIMx capture/compare mode reigster | TIMx_CCMR1
+
+    // OC1M: Output compare 1 mode bits 6:4
+    TIM2->CCMR1 &= ~(0b111 << 6);
+    TIM2->CCMR1 |= (0b0110 << 6); //  PWM mode 1 - In upcounting, channel 1 is active as long as TIMx_CNT<TIMx_CCR1
+                                  //  else inactive
+    
+    // OC1PE: Output compare 1 preload enable bit 3
+    TIM2->CCMR1 |= (0b1 << 3);  // Preload register enabled
+
+    // CC1S: Capture/Compare 1 selection bits 1:0
+    TIM2->CCMR1 &= ~(0b11);
+
+    //-----------------------//  TIMx prescaler | TIMx_PSC
+    
+    // PSC[15:0] Prescaler value
+    TIM2->PSC &= ~(0b1 << 15); // Set Prescaler to 0
+
+    //-----------------------// TIMx auto-reload register | TIMx_ARR
+    // Set Frequency
+    TIM2->ARR = freq;
+    
+    //-----------------------// TIMx event generation register | TIMx_EGR
+
+    // UG: Update generation bit 0
+    TIM2->EGR |= 0b1; // enable UG bit and generate update
+    TIM2->EGR &= 0b0; // disable UG bit
+    
 
     if (freq >= 0) {
-        TIM2->//CR1, bit CEN |= 1; // SMTH LIKE THIS
+        TIM2->CR1 &= 0b1;
     }
 }
 
